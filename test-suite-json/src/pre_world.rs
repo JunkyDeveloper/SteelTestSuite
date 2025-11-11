bb7d35d940aa8c33c2016429968df34b352cdf94
= <=>
2025-11-11 18:36:37 +0100

    some world settings

--- test-suite-json/src/pre_world.rs
+++ test-suite-json/src/pre_world.rs
@@ -6,3 +6,15 @@ use crate::blocks::BlockPosition;
 pub struct PreWorld {
     pub blocks: Vec<BlockPosition>,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct WorldEnvironment{
+    // The time of day. 0 is sunrise, 6000 is mid day, 12000 is sunset, 18000 is mid night, 24000 is the next day's 0. This value keeps counting past 24000 and does not reset to 0.
+    pub day_time: i64,
+    // #[serde(with = "serde_enum_as_integer")] TODO
+    // pub difficulty: Difficulty,
+    // pub game_rules: GameRuleRegistry,
+    #[serde(rename = "allowCommands")]
+    pub allow_commands: bool,
+}
